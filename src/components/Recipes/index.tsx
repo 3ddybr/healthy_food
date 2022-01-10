20ac11787f30ff81114cafc8d49386503e1cf71b
Alberto Consoli de Araujo <68253941+3ddybr@users.noreply.github.com>
2022-01-10 07:37:05 -0300

    components recipes ok

--- src/components/Recipes/index.tsx
+++ src/components/Recipes/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { RecipesCard } from '../RecipesCard';
 
 import styles from './styles.module.scss';
+import comidaImg1 from '../../assets/comida_1.svg';
+import comidaImg2 from '../../assets/comida_2.svg';
+import comidaImg3 from '../../assets/comida_3.svg';
+import comidaImg4 from '../../assets/comida_4.svg';
 
 export function Recipes() {
   return (
@@ -12,7 +17,15 @@ export function Recipes() {
           Vokalia and Consonantia, there live the blind texts.
         </p>
       </div>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <RecipesCard title="Broccoli Salad with Bacon" srcImage={comidaImg1} />
+        <RecipesCard title="Classic Beef Burgers" srcImage={comidaImg2} />
+        <RecipesCard title="Classic Potato Salad" srcImage={comidaImg3} />
+        <RecipesCard
+          title="Cherry Cobbler on the Grill"
+          srcImage={comidaImg4}
+        />
+      </div>
     </div>
   );
 }
